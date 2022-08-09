a8bfbda643be49617e278f7b824c628b49b1d3ed
basile <basile.vernouillet@origins-digital.com>
2022-08-09 11:57:31 +0200

    fix: build error after updating Mantine

--- pages/playlists/[playlistId].tsx
+++ pages/playlists/[playlistId].tsx
@@ -11,10 +11,10 @@ import { VideosList } from '../../src/features/videos';
 import { Video } from '../../src/features/videos/fetcher';
 
 const tabsMap: {
-  [key: string]: number;
+  [key: string]: string;
 } = {
-  display: 0,
-  videos: 1,
+  display: 'display',
+  videos: 'videos',
 };
 function PlaylistId() {
   const router = useRouter();
@@ -24,7 +24,7 @@ function PlaylistId() {
       <LoadingOverlay visible={isLoading} />
       {data && (
         <Tabs
-          active={tabsMap[router.query.tabs as string]}
+          value={tabsMap[router.query.tabs as string]}
           onTabChange={(i) =>
             router.push({
               pathname: router.asPath.split('?')[0],
@@ -32,11 +32,18 @@ function PlaylistId() {
             })
           }
         >
-          <Tabs.Tab label="Display">
+          <Tabs.List>
+            {Object.values(tabsMap).map((tab) => (
+              <Tabs.Tab value={tab} key={tab}>
+                {tab.toUpperCase()}
+              </Tabs.Tab>
+            ))}
+          </Tabs.List>
+          <Tabs.Tab value="display">
             <Edito {...data} />
             <Display {...data} />
           </Tabs.Tab>
-          <Tabs.Tab label="Videos">
+          <Tabs.Tab value="videos">
             <VideosList videos={data.Videos as Video[]} />
           </Tabs.Tab>
         </Tabs>
